d2fe51662754f406c81d0dee3e36f573787adc27
Shreya Shekhar <shreyashekhar@berkeley.edu>
2021-02-21 21:22:55 -0800

    removed staticfile settings

--- gettingstarted/base.py
+++ gettingstarted/base.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    #"django.contrib.staticfiles",
     'hello',
     "rest_framework", 
     "rest_framework.authtoken", 
@@ -145,10 +145,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATIC_URL = "/static/"
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 
 
